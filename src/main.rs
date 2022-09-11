ce75e359abb1336610a1386f762414d35a1515bb
katekyy <deffkatek@gmail.com>
2022-09-11 23:33:32 +0200

    Fix: Issue #1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::fs;
-use std::{thread, time};
+use std::path::Path;
+use std::{thread, time::Duration};
 
 fn main() {
-	let five_seconds = time::Duration::from_millis(5000);
-
 	let mut data: String;
+    let mut i: i32 = 0;
+	let mut b: bool = false; 
+
+	b = Path::new("/proc/acpi/button/lid/LID").exists();
+	for i in 0.. {
+		if b {break};
+		b = Path::new(&format!("/proc/acpi/button/lid/LID{}", i)).exists();
+	}
+
 	loop {
-		data = fs::read_to_string("/proc/acpi/button/lid/LID0/state").expect("Unable to read file").trim().to_string();
-		let my_vec = data.split_whitespace().collect::<Vec<&str>>();
+		if !b {
+			data = fs::read_to_string(&format!("/proc/acpi/button/lid/LID{}/state", i)).expect("Unable to read file").trim().to_string();
+		} else {
+			data = fs::read_to_string("/proc/acpi/button/lid/LID/state").expect("Unable to read file").trim().to_string();
+		}
+
+		let my_vec: Vec<&str> = data.split_whitespace().collect::<Vec<&str>>();
 
 		if my_vec[1] == "closed" {
 			// hibernate if lid is closed
-			std::process::Command::new("systemctl").arg("suspend").status().expect("Failed to suspend.");
+			std::process::Command::new("systemctl").arg("suspend").status().expect("Failed to suspend");
 		}
-		thread::sleep(five_seconds);
+		thread::sleep(Duration::from_millis(5000));
 	}
 }
