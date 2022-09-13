61fbd647882f507a712c77370ec7bf21659bd8e7
katekyy <deffkatek@gmail.com>
2022-09-13 18:29:53 +0200

    Fix: Issue #1

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,32 @@ use std::{thread, time::Duration};
 fn main() {
 	let mut data: String;
     let mut i: i32 = 0;
-	let mut b: bool = false; 
+	let mut b: bool = false;
+	let mut quit: bool = false; 
 
-	b = Path::new("/proc/acpi/button/lid/LID").exists();
+	// declare 2 vars with the same values in one line
+	let path = Path::new("/proc/acpi/button/lid/LID/").exists();
+	(b, quit) = (path, path);
+
 	for i in 0.. {
-		if b {break};
-		b = Path::new(&format!("/proc/acpi/button/lid/LID{}", i)).exists();
+		if quit {break};
+		quit = Path::new(&format!("/proc/acpi/button/lid/LID{}/", i)).exists();
 	}
 
 	loop {
-		if !b {
-			data = fs::read_to_string(&format!("/proc/acpi/button/lid/LID{}/state", i)).expect("Unable to read file").trim().to_string();
-		} else {
+		if b {
+			println!("{:?}", b);
 			data = fs::read_to_string("/proc/acpi/button/lid/LID/state").expect("Unable to read file").trim().to_string();
+		} else {
+			println!("{:?}", b);
+			data = fs::read_to_string(&format!("/proc/acpi/button/lid/LID{}/state", i)).expect("Unable to read file").trim().to_string();
 		}
 
 		let my_vec: Vec<&str> = data.split_whitespace().collect::<Vec<&str>>();
 
 		if my_vec[1] == "closed" {
 			// hibernate if lid is closed
-			std::process::Command::new("systemctl").arg("suspend").status().expect("Failed to suspend");
+			std::process::Command::new("echo").arg("closed").status().expect("Failed to suspend");
 		}
 		thread::sleep(Duration::from_millis(5000));
 	}
