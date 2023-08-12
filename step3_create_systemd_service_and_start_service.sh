8fe0a53b6d10d17ed09146a26034e5746bd6fd62
Elias Jonsson <e@ejon.eu>
2023-08-12 19:47:18 +0200

    fixed critical step3 bug making app unoperable. now runs as root

--- step3_create_systemd_service_and_start_service.sh
+++ step3_create_systemd_service_and_start_service.sh
@@ -6,30 +6,18 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Check if user argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <username>"
-  exit 1
-fi
-
-# Check if user exists
-if ! id -u "$1" > /dev/null 2>&1; then
-  echo "User $1 does not exist."
-  exit 1
-fi
-
 # Path to the systemd service file
-SERVICE_FILE="/etc/systemd/system/lidshutsleep@.service"
+SERVICE_FILE="/etc/systemd/system/lidshutsleep.service"
 
 # Write the service file
 cat <<EOL > "$SERVICE_FILE"
 [Unit]
-Description=Lid Shut Sleep Service for %i
+Description=Lid Shut Sleep Service
 After=display-manager.service
 
 [Service]
 ExecStart=/usr/local/bin/lidshutsleep
-User=%i
+User=root
 Restart=always
 
 [Install]
@@ -40,9 +28,9 @@ EOL
 systemctl daemon-reload
 
 # Enable the service for the specified user
-systemctl enable "lidshutsleep@$1"
+systemctl enable "lidshutsleep"
 
 # Start the service for the specified user
-systemctl start "lidshutsleep@$1"
+systemctl start "lidshutsleep"
 
-echo "Service lidshutsleep@$1 installed and started successfully."
+echo "Service lidshutsleep installed and started successfully."
